abdc9004b6e6b9dca44d2cddb01f239a038891e8
Oleh Vasiliev <olegvasiliev1991@gmail.com>
2020-10-09 20:03:43 +0300

    added base components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("HI")
+import React from "react";
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router} from "react-router-dom";
+
+import App from "./components/app";
+import ErrorBoundry from "./components/error-boundry/error-boundry";
+import BookstoreService from "./services/bookstore-servise";
+import { BookstoreServiceProvider} from "./components/bookstore-service-context";
+
+import store from './store';
+import {renderIntoDocument} from "react-dom/test-utils";
+
+const bookstoreService = new BookstoreService();
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <ErrorBoundry>
+      <BookstoreServiceProvider value={bookstoreService}>
+        <Router>
+          <App />
+        </Router>
+    </BookstoreServiceProvider>
+    </ErrorBoundry>
+  </Provider>,
+    document.getElementById('root')
+);
