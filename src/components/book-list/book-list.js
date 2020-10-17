2601b2bc4e864f54c85af62873def8438d8d78fa
Oleh Vasiliev <olegvasiliev1991@gmail.com>
2020-10-17 17:30:33 +0300

    added function connect() to connect component to store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import BookListItem from "../book-list-item";
+import { connect } from 'react-redux';
 import './book-list.css'
 
 class BookList extends Component {
@@ -20,4 +21,8 @@ class BookList extends Component {
   }
 }
 
-export default BookList;
+const mapStateProps = ({ books }) => {
+  return { books };
+};
+
+export default connect(mapStateProps)(BookList);
